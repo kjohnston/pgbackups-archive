15d5a9d1d29d7cd36a891806bab36020d8c13d4b
Robert Bousquet <rbousquet@newleaders.com>
2012-08-03 00:45:05 -0700

    Autoload rake task into Rails2.x once the gem has been loaded, updated README

--- lib/pgbackups-archive/railtie.rb
+++ lib/pgbackups-archive/railtie.rb
@@ -1,10 +1,13 @@
-if defined?(Rails) && defined?(Rails::Railtie)
-  module PgbackupsArchive
-    class Railtie < Rails::Railtie
-      railtie_name :pgbackups_archive
-
-      rake_tasks do
-        load "tasks/pgbackups_archive.rake"
+if defined?(Rails)
+  if Rails.version < "3"
+    load "tasks/pgbackups_archive.rake"
+  else
+    module PgbackupsArchive
+      class Railtie < Rails::Railtie
+        railtie_name :pgbackups_archive
+        rake_tasks do
+          load "tasks/pgbackups_archive.rake"
+        end
       end
     end
   end
